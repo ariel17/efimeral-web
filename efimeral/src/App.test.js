18193c0b01d8b776067df1302d84c7373889b218
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-05-18 17:34:47 -0300

    Changes test to match reality.

--- efimeral/src/App.test.js
+++ efimeral/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders spinner', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const spinner = screen.getByTestId('spinner')
+  expect(spinner).toBeInTheDocument();
 });
