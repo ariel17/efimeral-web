e28430ac5db442066a4d6a507dcbea7d9176d375
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-05-19 16:24:20 -0300

    Changes CNAME registry to A. Refs #4

--- cdk/lib/web-stack.ts
+++ cdk/lib/web-stack.ts
@@ -9,14 +9,24 @@ export class WebStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const zone = new route53.HostedZone(this, 'web-hosted-zone', {
+    const zone = new route53.PublicHostedZone(this, 'web-hosted-zone', {
       zoneName: domainName,
     });
 
-    new route53.CnameRecord(this, 'cname-web', {
+    new route53.ARecord(this, 'cname-web', {
       zone: zone,
       recordName: domainName,
-      domainName: 'ariel17.github.io',
+      target: route53.RecordTarget.fromIpAddresses(
+        '185.199.108.153',
+        '185.199.109.153',
+        '185.199.110.153',
+        '185.199.111.153',
+      ),
+    });
+
+    new cdk.CfnOutput(this, 'ns-servers', {
+      description: 'NS servers',
+      value: cdk.Fn.join(',', zone.hostedZoneNameServers || []),
     });
   }
 }
--- cdk/test/web.test.ts
+++ cdk/test/web.test.ts
@@ -3,7 +3,7 @@ import { Template } from 'aws-cdk-lib/assertions';
 import * as WebStack from '../lib/web-stack';
 
 
-test('CNAME registry created', () => {
+test('A registry created', () => {
   const app = new cdk.App();
     // WHEN
   const stack = new WebStack.WebStack(app, 'MyTestStack');
@@ -16,7 +16,13 @@ test('CNAME registry created', () => {
 
   template.hasResourceProperties('AWS::Route53::RecordSet', {
     Name: WebStack.domainName+'.',
-    ResourceRecords: ['ariel17.github.io'],
+    Type: 'A',
+    ResourceRecords: [
+        '185.199.108.153',
+        '185.199.109.153',
+        '185.199.110.153',
+        '185.199.111.153',
+    ],
     TTL: "1800",
   });
 });
