a13caddd34bd4f048d47e94a4e26cdbd5230e448
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-05-24 16:04:20 -0300

    Adds const var for stack. Refs efimeral-api#7

--- cdk/bin/web.ts
+++ cdk/bin/web.ts
@@ -6,7 +6,7 @@ import { WebStack } from '../lib/web-stack';
 const stackName = 'EfimeralWebStack';
 
 const app = new cdk.App();
-new WebStack(app, stackName, {
+const stack = new WebStack(app, stackName, {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
 cdk.Tags.of(stack).add('StackType', stackName);
