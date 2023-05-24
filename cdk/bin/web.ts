63d206456753a8c9ff3aec9814bf4acea0432c96
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-05-24 16:03:18 -0300

    Adds tags for web stack. Refs efimeral-api#7

--- cdk/bin/web.ts
+++ cdk/bin/web.ts
@@ -3,7 +3,10 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { WebStack } from '../lib/web-stack';
 
+const stackName = 'EfimeralWebStack';
+
 const app = new cdk.App();
-new WebStack(app, 'EfimeralWebStack', {
+new WebStack(app, stackName, {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
+cdk.Tags.of(stack).add('StackType', stackName);
